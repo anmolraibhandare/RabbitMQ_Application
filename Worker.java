13c29c8310be3eebf24efa0b24970bdfad121c4b
Anmol Raibhandare <raibhandare.anmol@gmail.com>
2021-06-13 23:24:33 -0700

    Create Worker.java

---
+++ Worker.java
@@ -0,0 +1,35 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Worker {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+
+        //  Fake a second of work for every dot in the message body
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+          
+            System.out.println(" [x] Received '" + message + "'");
+            try {
+              doWork(message);
+            } finally {
+              System.out.println(" [x] Done");
+            }
+          };
+          boolean autoAck = true; // acknowledgment is covered below
+          channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+    }
+}
