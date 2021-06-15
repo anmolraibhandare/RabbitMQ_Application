266f5647390f593c3769a1a8d7f247f1b5ba2fd7
Anmol Raibhandare <raibhandare.anmol@gmail.com>
2021-06-15 16:16:48 -0700

    Updated send and recv
    Added comments

--- Recv.java
+++ Recv.java
@@ -2,12 +2,17 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
+// DeliverCallback interface is used to buffer the messages pushed to us by the server
 
 public class Recv {
 
+    // Declare queue
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
+        // Create a connection
+        // No try-with-resource - we want the process to stay alive 
+        // while the consumer is listening asynchronously for messages to arrive
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
@@ -16,6 +21,8 @@ public class Recv {
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
+        // Since the messafes are pushed asynchronously, we provide a callback
+        // in form of object that will buffer messages until we are ready to use them
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + message + "'");
--- Send.java
+++ Send.java
@@ -1,3 +1,5 @@
+// The RabbitMQ Java client is in the central Maven repository, 
+// with the groupId com.rabbitmq and the artifactId amqp-client.
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -6,15 +8,29 @@ import java.nio.charset.StandardCharsets;
 
 public class Send {
 
+    // Name of message buffer queue
     private final static String QUEUE_NAME = "hello";
-
     public static void main(String[] argv) throws Exception {
+        // Create a connection
         ConnectionFactory factory = new ConnectionFactory();
+
+        // Connect a RabbitMQ node on the local machine
+        // if we want to connect to different machine - specify hostname/IP address
         factory.setHost("localhost");
+
+        // We use a try-with-resources statement because both Connection and Channel implement java.io.Closeable
+        // This way we don't need to close them explicitly in our code
         try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+
+            // Create a channel
+            Channel channel = connection.createChannel()) {
+            
+            // Declare a queue - created only if it does not exist
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             String message = "Hello World!";
+            
+            // Publish the message
+            // The message content is a byte array, so you can encode whatever you like there
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
             System.out.println(" [x] Sent '" + message + "'");
         }
